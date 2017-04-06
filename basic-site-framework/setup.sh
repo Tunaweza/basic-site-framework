661a00836339c5b7dcf7867b69785608a0888d5f
McAttack <markhatchell@users.noreply.github.com>
2017-04-06 18:19:18 -0400

    Update setup.sh

--- basic-site-framework/setup.sh
+++ basic-site-framework/setup.sh
@@ -4,4 +4,7 @@ python manage.py loaddata pages.json
 python manage.py loaddata blog.json
 python manage.py loaddata menu.json
 python manage.py loaddata snippets.json
+cd staticwork/
+bower install
+cd ..
 python manage.py collectstatic
